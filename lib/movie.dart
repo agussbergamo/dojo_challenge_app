0dd011311a4c7e2c38a67d092afe18578da524ec
Agustina <asbergamo@alumnos.exa.unicen.edu.ar>
2025-03-27 11:09:28 -0300

    Add popular movies request

---
+++ lib/movie.dart
@@ -0,0 +1,46 @@
+class Movie {
+  final bool adult;
+  final List<int> genreIds;
+  final int id;
+  final String originalLanguage;
+  final String originalTitle;
+  final String overview;
+  final double popularity;
+  final String releaseDate;
+  final String title;
+  final bool video;
+  final double voteAverage;
+  final int voteCount;
+
+  Movie({
+    required this.adult,
+    required this.genreIds,
+    required this.id,
+    required this.originalLanguage,
+    required this.originalTitle,
+    required this.overview,
+    required this.popularity,
+    required this.releaseDate,
+    required this.title,
+    required this.video,
+    required this.voteAverage,
+    required this.voteCount,
+  });
+
+  factory Movie.fromJson(json) {
+    return Movie(
+      adult: json['adult'],
+      genreIds: json['genre_ids'].cast<int>(),
+      id: json['id'],
+      originalLanguage: json['original_language'],
+      originalTitle: json['original_title'],
+      overview: json['overview'],
+      popularity: json['popularity'].toDouble(),
+      releaseDate: json['release_date'],
+      title: json['title'],
+      video: json['video'],
+      voteAverage: json['vote_average'].toDouble(),
+      voteCount: json['vote_count'],
+    );
+  }
+}
